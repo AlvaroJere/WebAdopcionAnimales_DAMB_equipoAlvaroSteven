d889cd2fdfe2d03a2a66d3391cb8907d575bebc8
AlvaroJere <alvarobarreto2007@gmail.com>
2026-04-30 11:52:07 +0200

    Mi primer commit con todo el avance

---
+++ src/app/componentes/web-adopcion-animales-dambequipo-alvaro-steven/web-adopcion-animales-dambequipo-alvaro-steven.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-web-adopcion-animales-dambequipo-alvaro-steven',
+  imports: [],
+  templateUrl: './web-adopcion-animales-dambequipo-alvaro-steven.component.html',
+  styleUrl: './web-adopcion-animales-dambequipo-alvaro-steven.component.css'
+})
+export class WebAdopcionAnimalesDAMBequipoAlvaroStevenComponent {
+
+}
---
+++ src/app/layout/navbar/navbar.component.ts
@@ -0,0 +1,24 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  lastScrollTop = 0;
+  isHeaderHidden = false;
+
+  // Este decorador detecta el scroll automáticamente
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > this.lastScrollTop && scrollTop > 100) {
+      this.isHeaderHidden = true; // Ocultar
+    } else {
+      this.isHeaderHidden = false; // Mostrar
+    }
+    this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+  }
+}
