2253c5288aa06546ef480a08a17306fa68790f80
AlvaroJere <alvarobarreto2007@gmail.com>
2026-04-30 18:52:59 +0200

    feat: generar componentes en carpeta pages y configurar rutas básicas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AdopcionComponent } from './pages/adopcion/adopcion.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Página que sale por defecto
+  { path: 'adopcion', component: AdopcionComponent },
+  { path: 'contacto', component: ContactoComponent },
+];
