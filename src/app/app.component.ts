7296f5be4418061767244b2659665679d83fb260
AlvaroJere <alvarobarreto2007@gmail.com>
2026-05-08 18:11:57 +0200

    feat(animations): implementar AOS para transiciones de entrada en scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as AOS from 'aos'; // Importamos AOS para las animaciones
 
 import { NavbarComponent } from './layout/navbar/navbar.component'; 
 import { FooterComponent } from './layout/footer/footer.component';
@@ -10,6 +11,15 @@ import { FooterComponent } from './layout/footer/footer.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'WebAdopcionAnimales_DAMBequipoAlvaroSteven';
+
+  ngOnInit() {
+    AOS.init({ // Inicializamos AOS con opciones personalizadas
+      duration: 600, // Duración de la animación en ms
+      once: true, // Animar solo una vez al hacer scroll
+      mirror: false, // No animar elementos al hacer scroll hacia arriba
+    });
+  }
+
 }
