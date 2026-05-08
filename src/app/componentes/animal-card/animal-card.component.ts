31d18ba636eae4354d09d67398ae428454ad1391
AlvaroJere <alvarobarreto2007@gmail.com>
2026-05-08 12:54:16 +0200

    feat: conectar AnimalCard con el servicio y añadir lógica de favoritos

--- src/app/componentes/animal-card/animal-card.component.ts
+++ src/app/componentes/animal-card/animal-card.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core'; 
+import { Component, inject, Input } from '@angular/core'; 
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Animal } from '../../animal.interface'; // Importamos la interface para tipar mejor el input
+import { AnimalService } from '../../servicios/animal.service'; // Importamos el servicio para usarlo si es necesario
 
 @Component({
   selector: 'app-animal-card',
@@ -10,5 +12,19 @@ import { RouterModule } from '@angular/router';
   styleUrl: './animal-card.component.css'
 })
 export class AnimalCardComponent {
-  @Input() animal: any; // Sin esto, [animal]="mascota" no funciona
+  @Input() animal: Animal; // Recibimos un objeto de tipo Animal para mostrar su información
+
+  private _animalService = inject(AnimalService); // Inyección del servicio por si necesitamos usarlo aquí
+
+  // Método para alternar favorito, que llamará al servicio
+  toggleFavorito(): void {
+    this._animalService.toggleFavorito(this.animal.id);
+  }
+  // Método para verificar si el animal es favorito, que llamará al servicio
+  esFavorito(): boolean {
+    return this._animalService.esFavorito(this.animal.id);
+  }
+
+
+
 }
